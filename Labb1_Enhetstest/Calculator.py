76e41510a55c2a24be3057dcb4e9a0b900cb8ba0
Samabed <sam.abed@live.se>
2022-11-30 00:40:24 +0100

    Updated the code with argparse

--- Labb1_Enhetstest/Calculator.py
+++ Labb1_Enhetstest/Calculator.py
@@ -1,57 +1,62 @@
 # Program make a simple calculator that can add, subtract, multiply and divide using functions
 
+import argparse
+
+
 class Calculator:
     def __init__(self):
         pass
 
+    # define functions
+    def add(self, x, y):
+        """This function adds two numbers"""
 
-# define functions
-def add(x, y):
-    """This function adds two numbers"""
+        return x + y
 
-    return x + y
+    def subtract(self, x, y):
+        """This function subtracts two numbers"""
 
+        return x - y
 
-def subtract(x, y):
-    """This function subtracts two numbers"""
+    def multiply(self, x, y):
+        """This function multiplies two numbers"""
 
-    return x - y
+        return x * y
 
+    def divide(self, x, y):
+        """This function divides two numbers"""
 
-def multiply(x, y):
-    """This function multiplies two numbers"""
-
-    return x * y
-
-
-def divide(x, y):
-    """This function divides two numbers"""
-
-    return x / y
+        return x / y
 
 
-# take input from the user
-print("Select operation.")
-print("1.Add")
-print("2.Subtract")
-print("3.Multiply")
-print("4.Divide")
+my_parser = argparse.ArgumentParser()
+my_parser.add_argument('--manual', action='store_true', default=False)
 
-choice = input("Enter choice(1/2/3/4):")
+args = my_parser.parse_args()
+if args.manual:
 
-num1 = int(input("Enter first number: "))
-num2 = int(input("Enter second number: "))
+    # take input from the user
+    print("Select operation.")
+    print("1.Add")
+    print("2.Subtract")
+    print("3.Multiply")
+    print("4.Divide")
 
-if choice == '1':
-    print(num1, "+", num2, "=", add(num1, num2))
+    choice = input("Enter choice(1/2/3/4):")
 
-elif choice == '2':
-    print(num1, "-", num2, "=", subtract(num1, num2))
+    num1 = int(input("Enter first number: "))
+    num2 = int(input("Enter second number: "))
 
-elif choice == '3':
-    print(num1, "*", num2, "=", multiply(num1, num2))
+    if choice == '1':
+        print(num1, "+", num2, "=", Calculator().add(num1, num2))
 
-elif choice == '4':
-    print(num1, "/", num2, "=", divide(num1, num2))
-else:
-    print("Invalid input")
+    elif choice == '2':
+        print(num1, "-", num2, "=", Calculator().subtract(num1, num2))
+
+    elif choice == '3':
+        print(num1, "*", num2, "=", Calculator().multiply(num1, num2))
+
+    elif choice == '4':
+        print(num1, "/", num2, "=", Calculator().divide(num1, num2))
+    else:
+        print("Invalid input")
