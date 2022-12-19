d8178c1b696ac89ac4c5b0f42cc139cefdb6662a
Samabed <sam.abed@live.se>
2022-12-19 22:13:41 +0100

    Find the search input field and search for a product

--- Labb2_Selenium/Website.py
+++ Labb2_Selenium/Website.py
@@ -1,6 +1,15 @@
+from selenium.webdriver import Keys
+from selenium.webdriver.common.by import By
+
+
 class Webbhallen:
     def __init__(self, driver):
         self.driver = driver
 
     def go_to_product_page(self):
         self.driver.get('https://www.webhallen.com/se/product/335143-Apple-AirTag-1-Pack')
+
+    def search_for_product(self, product_name):
+        search_input = self.driver.find_element(By.XPATH, "//input[@placeholder='Sök bland över 16 000 produkter']")
+        search_input.send_keys(product_name)
+        search_input.send_keys(Keys.ENTER)
