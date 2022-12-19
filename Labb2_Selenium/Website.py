1ada8ee52c06eeb87c5b31142d42eed2673e6757
Samabed <sam.abed@live.se>
2022-12-19 21:59:34 +0100

    Navigate to the AirTag product page

--- Labb2_Selenium/Website.py
+++ Labb2_Selenium/Website.py
@@ -2,3 +2,5 @@ class Webbhallen:
     def __init__(self, driver):
         self.driver = driver
 
+    def go_to_product_page(self):
+        self.driver.get('https://www.webhallen.com/se/product/335143-Apple-AirTag-1-Pack')
