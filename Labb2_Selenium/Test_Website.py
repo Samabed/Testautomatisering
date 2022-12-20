4a6d45de66356a070fa1abe6b2a4727c827bfaaa
Samabed <sam.abed@live.se>
2022-12-20 23:26:39 +0100

    Add testcase 1

--- Labb2_Selenium/Test_Website.py
+++ Labb2_Selenium/Test_Website.py
@@ -1,5 +1,7 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from Labb2_Selenium.Website import Webbhallen
 
 
 @pytest.fixture
@@ -7,4 +9,16 @@ def driver():
     # Skapar en webdriver-instans
     driver = webdriver.Chrome()
     yield driver
+
+
+def test_search_for_product(driver):
+    webbhallen = Webbhallen(driver)
+    webbhallen.go_to_product_page()
+    webbhallen.search_for_product('Apple AirTag')
+
+    # Kontrollera att sökresultatet visas på sidan
+    search_results = driver.find_elements(By.XPATH, "//div[@class='search-results']")
+    assert len(search_results) > 0
+
+
 
